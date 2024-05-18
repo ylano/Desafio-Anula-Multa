7b4f24bcd6d4b1826ae1668908f75dda749eb15c
Francisco Ylano <137581500+ylano@users.noreply.github.com>
2024-05-18 15:14:27 -0300

    Add files via upload

---
+++ pages/api/tasks/index.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    const tasks = await prisma.task.findMany();
+    res.json(tasks);
+  } else if (req.method === 'POST') {
+    const { title, description } = req.body;
+    const newTask = await prisma.task.create({
+      data: {
+        title,
+        description,
+      },
+    });
+    res.json(newTask);
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+}
